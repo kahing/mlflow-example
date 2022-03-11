eaed9a04ff68bc3a5ab75f8a729eb569353fbf7f
Ka-Hing Cheung <khc@hxbc.us>
2022-03-11 14:41:59 -0800

    typo

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # DB_COMMAND_METADATA {"plotOptions":{"displayType":"table","xColumns":null,"yColumns":null,"pivotColumns":null,"pivotAggregation":null,"customPlotOptions":{}},"nuid":"4e36ccea-247a-436d-bdbc-127b1c0ab5b7"}
-# MAGIC %md some heading
+# MAGIC %md some headings
 
 # COMMAND ----------
 
