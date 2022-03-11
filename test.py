63e2826a80a7a854f48a4260e888f855c6f77893
kahing <kahing>
2022-03-11 22:41:37 +0000

    test

---
+++ test.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# DB_COMMAND_METADATA {"plotOptions":{"displayType":"table","xColumns":null,"yColumns":null,"pivotColumns":null,"pivotAggregation":null,"customPlotOptions":{}},"nuid":"4e36ccea-247a-436d-bdbc-127b1c0ab5b7"}
+# MAGIC %md some heading
+
+# COMMAND ----------
+
+# DB_COMMAND_METADATA {"plotOptions":{"displayType":"table","xColumns":null,"yColumns":null,"pivotColumns":null,"pivotAggregation":null,"customPlotOptions":{}},"nuid":"8f1fffd1-6457-4da2-bdb6-24019ad83c9e"}
+# MAGIC %sql SELECT 1, 2, 3
